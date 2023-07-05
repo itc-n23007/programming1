e23ce25af89151f877a1f0d8d5492532a1752115
itc-n23007 <n23007@std.it-college.ac.jp>
2023-07-05 08:00:44 +0000

    リスト

---
+++ chapter2/Q2_7_1.py
@@ -0,0 +1,6 @@
+list = []
+list.append("orange")
+list.append("apple")
+list.append("grape")
+list.append("banana")
+print(list)
---
+++ chapter2/Q2_7_2.py
@@ -0,0 +1,5 @@
+list = ["orange", "apple", "grape", "banana"]
+list.sort()
+print(list)
+list.sort(reverse=True)
+print(list)
