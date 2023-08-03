5668529815b97855092f2948f0668d88df02af40
itc-n23007 <n23007@std.it-college.ac.jp>
2023-08-03 04:22:44 +0000

    forbunnkiso

--- chapter3/Q3_3_1.py
+++ chapter3/Q3_3_1.py
@@ -1,1 +1,6 @@
 list = ["tokyo", "osaka", "fukuoka", "aichi", "kyoto", "chiba", "saitama", "gunma"]
+list2 = []
+for i in list:
+    if len(i) <= 6:
+        list2.append(i)
+print(list2)
---
+++ chapter3/Q3_3_2.py
@@ -0,0 +1,6 @@
+list = [1, 1, 2, 3, 5, 8, 13]
+x = 0
+for i in list:
+    if i % 2 != 0:
+        x += i
+print(x)
